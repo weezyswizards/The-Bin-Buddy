c78c0923d80e7539bf0a1987caa3d361c0096190
Labib Sarwar <labib.swr@gmail.com>
2024-08-03 15:54:38 -0400

    Update modelcode.py

---
+++ model/modelcode.py
@@ -0,0 +1,8 @@
+import tensorflow as tf 
+from tf import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+
